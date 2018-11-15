440bb823c439e3f9675ff6f18f4ec68b79ad9fb2
Guilherme Gois <guijocargo@gmail.com>
2018-11-15 10:46:59 -0200

    Refatoração do objeto de calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,38 +1,54 @@
 var Calculadora = {
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  DIVISAO: '/',
+  MULTIPLICACAO: '*',
+
   adicionar: function (n1, n2) {
-    if (isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-
-    n1 = parseFloat(n1);
-    n2 = parseFloat(n2);
-
-    return n1 + n2;
+    return Calculadora.calcular(n1, n2, Calculadora.ADICAO);
   },
   dividir: function (n1, n2) {
-    if (isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-
     if (n2 == 0) {
       return 'Erro';
     }
 
-    return n1 / n2;
+    return Calculadora.calcular(n1, n2, Calculadora.DIVISAO);
   },
   multiplicar: function (n1, n2) {
-    if (isNaN(n1) || isNaN(n2)) {
-      return 0;
-    }
-    
-    return n1 * n2;
+    return Calculadora.calcular(n1, n2, Calculadora.MULTIPLICACAO);
   },
   subtrair: function (n1, n2) {
+    return Calculadora.calcular(n1, n2, Calculadora.SUBTRACAO);
+  },
+  calcular: function (n1, n2, op) {
+    var resultado = 0;
+
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+
     if (isNaN(n1) || isNaN(n2)) {
       return 0;
     }
 
-    return n1 - n2;
+    switch (op) {
+      case Calculadora.ADICAO:
+        resultado = n1 + n2;
+        break;
+      case Calculadora.SUBTRACAO:
+        resultado = n1 - n2;
+        break;
+      case Calculadora.DIVISAO:
+        resultado = n1 / n2;
+        break;
+      case Calculadora.MULTIPLICACAO:
+        resultado = n1 * n2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+
+    return resultado;
   }
 }
 
